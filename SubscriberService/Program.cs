dae74195b71789a44540d4a7034698c2b240d53b
nurromero <nuriyeerdogan38@gmail.com>
2025-11-04 09:57:16 +0100

    feat: routing in program.cs

--- SubscriberService/Program.cs
+++ SubscriberService/Program.cs
@@ -2,23 +2,21 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using SubscriberService.Data;
 using SubscriberService.Models;
 
-//https://localhost:7084/swagger/index.html
+// https://localhost:7084/swagger/index.html
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<SubscriberService.Data.SubscriberDbContext>(options =>
+builder.Services.AddDbContext<SubscriberDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 
 var app = builder.Build();
 
 var isEnabled = builder.Configuration.GetValue<bool>("SubscriberServiceEnabled", true);
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,26 +30,26 @@ if (app.Environment.IsDevelopment())
 
 if (isEnabled)
 {
-    var subscribers = new List<Subscriber>();
-
-    app.MapPost("/api/subscribers", (Subscriber newSub) =>
+    // Database-backed endpoints
+    app.MapPost("/api/subscribers", async (Subscriber newSub, SubscriberDbContext db) =>
     {
-        newSub.Id = subscribers.Count + 1;
         newSub.CreatedAt = DateTime.UtcNow;
-        subscribers.Add(newSub);
+        db.Subscribers.Add(newSub);
+        await db.SaveChangesAsync();
         return Results.Created($"/api/subscribers/{newSub.Id}", newSub);
     });
 
-    app.MapGet("/api/subscribers", () =>
+    app.MapGet("/api/subscribers", async (SubscriberDbContext db) =>
     {
-        return Results.Ok(subscribers);
+        var allSubs = await db.Subscribers.ToListAsync();
+        return Results.Ok(allSubs);
     });
 }
 else
 {
+    // Service disabled (release toggle)
     app.MapGet("/{*path}", () => Results.StatusCode(503));
     app.MapPost("/{*path}", () => Results.StatusCode(503));
 }
-
 
 app.Run();
